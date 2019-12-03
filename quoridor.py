152769b4941ceec7bb4249194b613aa960120ec8
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2019-12-02 22:58:34 -0500

    fini1

--- quoridor.py
+++ quoridor.py
@@ -186,10 +186,10 @@ class Quoridor:
             p2 = (a-1, b)
             if p1 or p2 in self.partie['état']['murs']['horizontaux']:
                 raise QuoridorError
-        if orientation == 'horizontal':
+        if orientation == 'vertical':
             p1 = (a, b+1)
             p2 = (a, b-1)
-            if p1 or p2 in self.partie['état']['murs']['horizontaux']:
+            if p1 or p2 in self.partie['état']['murs']['verticaux']:
                 raise QuoridorError
         # si la position est invalide pour cette orientation
         if not((2 <= a <= 9) and (1 <= b <= 8)) and orientation == 'vertical':
